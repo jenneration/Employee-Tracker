208c8d97ca1c972c4baeba0c04231fef8b0647a6
Jenner Garcia <jenner.garcia@gmail.com>
2021-02-12 23:00:33 -0500

    correction: all views updated

--- server.js
+++ server.js
@@ -86,25 +86,12 @@ const startTracker = () => {
 
 
 //****************READ*************************
+
+
+//TO DO: View Departments and budgets based on employees in each 
+
+
 //View All Department: name and id
-
-//View Departments and budgets based on employees in each 
-TODO
-const viewDeptBudgets = () => {
-    conn.query("SELECT department.name AS department, role.salary FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY department ASC"),
-        conn.query('SELECT name FROM department ORDER BY name ASC')
-
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        console.table(res);
-        startTracker();
-    })
-
-}
-
-
-
-
 const viewAllDepartments = () => {
     connection.query("SELECT id AS Dept_ID, name AS Department FROM department", (err, res) => {
         if (err) throw err;
